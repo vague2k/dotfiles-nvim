3b829f734a8b41b3f7fe1a80dd467932e4723245
albert <ilovedrawing056@gmail.com>
2024-05-02 03:01:27 -0400

    working on huez 1.0

--- lua/vague/plugins/huez.lua
+++ lua/vague/plugins/huez.lua
@@ -6,7 +6,18 @@ return {
         "grapp-dev/nui-components.nvim",
     },
     config = function()
-        require("huez").setup({})
+        require("huez").setup({
+            fallback = "zenbones",
+        })
         vim.keymap.set("n", "<leader>co", ":Huez<CR>", {})
+
+        --- OLD CONFIG
+        -- require("huez").setup({
+        --     fallback = "zenbones",
+        -- })
+        -- local colorscheme = require("huez.api").get_colorscheme()
+        -- vim.cmd("colorscheme " .. colorscheme)
+        --
+        -- vim.keymap.set("n", "<leader>co", ":Huez<CR>", {})
     end,
 }
