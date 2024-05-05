c0a3796ab2e9e829c8e01f51ec9167c80ce5c33e
albert <ilovedrawing056@gmail.com>
2024-05-05 12:19:08 -0400

    update lualine

--- lua/vague/plugins/lualine.lua
+++ lua/vague/plugins/lualine.lua
@@ -4,6 +4,28 @@ return {
         { "nvim-tree/nvim-web-devicons", lazy = true },
     },
     config = function()
-        require("lualine").setup({})
+        require("lualine").setup({
+            options = {
+                component_separators = { left = "", right = "" },
+                section_separators = { left = "", right = "" },
+                always_divide_middle = false,
+            },
+            sections = {
+                lualine_a = { "mode" },
+                lualine_b = { "branch", "diff", "diagnostics" },
+                lualine_c = { "filename" },
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = {},
+            },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = {},
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = {},
+            },
+        })
     end,
 }
