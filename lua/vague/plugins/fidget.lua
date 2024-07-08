be5da14f94b8592fa5471c7c280fe43de9b78423
vague2k <ilovedrawing056@gmail.com>
2024-07-07 21:00:58 -0400

    use fidget instead of notify

--- lua/vague/plugins/fidget.lua
+++ lua/vague/plugins/fidget.lua
@@ -102,12 +102,15 @@ return {
 
             -- Options related to logging
             logger = {
-                level = vim.log.levels.WARN, -- Minimum logging level
+                level = vim.log.levels.INFO, -- Minimum logging level
                 max_size = 10000, -- Maximum log file size, in KB
                 float_precision = 0.01, -- Limit the number of decimals displayed for floats
                 -- Where Fidget writes its logs to
                 path = string.format("%s/fidget.nvim.log", vim.fn.stdpath("cache")),
             },
         })
+
+        -- Let's other plugins that use vim.notify to display message to use notify
+        vim.notify = require("fidget").notify
     end,
 }
